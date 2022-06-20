4e7330de26cadba3762d5941dc11c56329d0cd19
VictorVM7 <vicmagalhaes20@gmail.com>
2022-06-20 20:17:13 -0300

    Adicionei o link home e o ptBR

--- scripts/pesquisa.js
+++ scripts/pesquisa.js
@@ -1,9 +1,11 @@
 // Chave da api do TMDB
+const ptBR = '&language=pt-BR';
 const API_KEY = 'api_key=250d01a9ef76763fba514ac9fcf6be88';
 const BASE_URL = 'https://api.themoviedb.org/3';
-const POPULAR_API = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
+const POPULAR_API = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY + ptBR;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
-const SEARCH_URL = BASE_URL + '/search/movie?' + API_KEY;
+const SEARCH_URL = BASE_URL + '/search/movie?' + API_KEY + ptBR;
+
 
 /*Ao carregar a página*/
 window.onload = () => {
