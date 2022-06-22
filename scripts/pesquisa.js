15bfd66792fa1e14ae73914a22247aa9be0739a8
VictorVM7 <vicmagalhaes20@gmail.com>
2022-06-21 23:34:12 -0300

    Trabalho final

--- scripts/pesquisa.js
+++ scripts/pesquisa.js
@@ -10,11 +10,58 @@ onload = () => {
     const main = document.getElementById('main');
     const form = document.getElementById('form');
     const search = document.getElementById('search');
+    const botao = document.getElementById('botaoProcurar');
+    const overlayContent = document.getElementById('overlay-content');
+
+    form.addEventListener('submit', (evento) => {
+        evento.preventDefault();
+
+        const itemProcurado = search.value;
+        if (itemProcurado == ""){
+            getFilmes(POPULAR_API);
+        }
+        else{
+            // JUNTA O ITEM PESQUISADO COM O ITEM PESQUISADO PELO USER
+            const FILME_PROCURADO = SEARCH_URL + '&query=' + itemProcurado;
+            // PEGA OS DADOS DA URL FILME PROCURADO E TESTA OS VALORES
+            fetch(FILME_PROCURADO).then(res => res.json()).then(dados =>{
+                if (dados.results.length == []){
+                    alert("Não há nenhum filme com esse nome");
+                }
+                else{
+                    getFilmes(FILME_PROCURADO);
+                }
+
+            })
+        } 
+    })
+
+    botao.addEventListener('click', (evento) => {
+        evento.preventDefault();
+
+        const itemProcurado = search.value;
+        if (itemProcurado == ""){
+            getFilmes(POPULAR_API);
+        }
+        else{
+            // JUNTA O ITEM PESQUISADO COM O ITEM PESQUISADO PELO USER
+            const FILME_PROCURADO = SEARCH_URL + '&query=' + itemProcurado;
+
+            // PEGA OS DADOS DA URL FILME PROCURADO E TESTA OS VALORES
+            fetch(FILME_PROCURADO).then(res => res.json()).then(dados =>{
+                if (dados.results.length == []){
+                    alert("Não há nenhum filme com esse nome");
+                }
+                else{
+                    getFilmes(FILME_PROCURADO);
+                }
+            })
+        }
+        
+    })
 }
 
-
 getFilmes(POPULAR_API);
-
 
 function getFilmes(url) {
     fetch(url).then(res => res.json()).then(dados =>{
@@ -23,12 +70,11 @@ function getFilmes(url) {
     })
 }
 
-
 function mostrarFilme(dados) {
     main.innerHTML = '';
 
     dados.forEach(filme =>{
-        const {title, poster_path, vote_average, overview} = filme;
+        const {title, poster_path, vote_average, overview, id} = filme;
         const elementoFilme = document.createElement('div');
         elementoFilme.classList.add('movie-card');
         elementoFilme.innerHTML = `
@@ -41,30 +87,60 @@ function mostrarFilme(dados) {
             <div class="review-filme">
                 ${overview}
                 <div class="div-btn-vermais">
-                    <button id="btn-vermais" type="button">Ver Mais</button>
+                    <button class="btn-vermais" id="${id}" type="button">Ver Mais</button>
                 </div> 
             </div>
         `
         main.appendChild(elementoFilme);
+        
+        document.getElementById(id).addEventListener('click', () => {
+            console.log(id);
+            openNav(filme);
+        })
     })
 }
 
-onload = () => {
-    const main = document.getElementById('main');
-    const form = document.getElementById('form');
-    const search = document.getElementById('search');
-    const botao = document.getElementById('botaoProcurar');
-
-    form.addEventListener('submit', (evento) => {
-        evento.preventDefault();
-        const itemProcurado = search.value;
-        const FILME_PROCURADO = SEARCH_URL + '&query=' + itemProcurado;
-    
-        if(FILME_PROCURADO){
-            getFilmes(FILME_PROCURADO);
-        } 
-        else {
-            getFilmes(POPULAR_API);
+/* Open when someone clicks on the span element */
+function openNav(filme) {
+    const overlayContent = document.getElementById('overlay-content');
+    let id = filme.id
+    fetch(BASE_URL + '/movie/' + id + '?' + API_KEY + ptBR).then(res => res.json()).then(dadosFilme => {
+        console.log(dadosFilme);
+        if (dadosFilme){
+            document.getElementById("myNav").style.width = "100%";
+            let results = [];
+            results.push(dadosFilme);
+            if(results){
+                results.forEach(imagem => {
+                    const {id, backdrop_path, vote_average, poster_path, title, overview, release_date, status} = imagem;
+                    overlayContent.innerHTML = `
+                    <div class="div-overlay">
+                        <div class="div-imagem">
+                            <img src="${IMG_URL + poster_path}" alt="${title}">
+                        </div>
+                        <div class="conteudo">
+                                <h1>${title}</h1>
+                                <h4> Data de Lançamento: </h4>${release_date}
+                                <h4> Status:</h4>${status}
+                                <h4> Nota do filme:</h4>${vote_average}
+                            <div class="review-filme">
+                                <h4> Resumo:</h4>${overview}
+                            </div>
+                        </div>
+                    </div>
+                    `;
+                });
+            } else {
+                overlayContent.innerHTML = `<h1>No Results</h1>`;
+            }
+            
         }
-    })
-}
+
+    });
+    
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
